482847a7abb63bb083c5329a853632ea872bbc5d
Nathana Kiwia <82622628+NathanaKiw@users.noreply.github.com>
2022-05-17 17:02:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,78 @@
+window.addEventListener('scroll', onScroll)
+onScroll()
+
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+  activateMenuAtCurrentSection(contact)
+}
+/*estado da aplicação ao executar a função não é o mesmo (função onScroll executa novamente a função activateMenuAtCurrentSection mas NÃO muda a const targetLine)*/
+function activateMenuAtCurrentSection(section) {
+  //linha que corta a metade da viewport - linha alvo
+  const targetLine = scrollY + innerHeight / 2
+
+  //verificar se a seção passou da linha
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  const sectionEndsAt = sectionTop + sectionHeight
+
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  //limites da seção
+  const sectionBoundaries =
+    sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if (sectionBoundaries) {
+    menuElement.classList.add('active')
+  }
+}
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 500) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+ScrollReveal({
+  origin: 'top' /*de onde vai aparecer top/bottom/left,right*/,
+  distance: '30px',
+  duration: 700
+}).reveal(`#home,
+#home img,
+#home .stats,
+#services,
+#services header,
+#services .card,
+#about,
+#about header,
+#about .content
+  `)
